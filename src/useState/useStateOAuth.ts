c7dd1adf5291f80be3c3a39ce78c055bddef72b4
linh1999 <linhct020328@gmail.gmail.com>
2025-12-02 12:02:56 +0700

    clearOAuth

--- src/useState/useStateOAuth.ts
+++ src/useState/useStateOAuth.ts
@@ -4,9 +4,11 @@ import { ResOAuth } from '@/interface/responseData/interfaceOAuth'
 interface State_ResOAuth {
     resOAuth: ResOAuth | undefined;
     setResOAuth: (data: ResOAuth) => void;
+    clearOAuth: () => void;
 }
 
 export const useState_ResOAuth = create<State_ResOAuth>((set) => ({
     resOAuth: undefined,
-    setResOAuth: (data) => set({ resOAuth: data })
-}))
+    setResOAuth: (data) => set({ resOAuth: data }),
+    clearOAuth: () => set({ resOAuth: undefined }),
+}));
